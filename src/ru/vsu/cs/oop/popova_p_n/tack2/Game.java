6252fe938186e77936a88007d2bee92836270158
Polina Popova <popovapolina2133@gmail.com>
2023-11-05 16:19:40 +0300

    Initial commit

---
+++ src/ru/vsu/cs/oop/popova_p_n/tack2/Game.java
@@ -0,0 +1,7 @@
+package ru.vsu.cs.oop.popova_p_n.tack2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/ru/vsu/cs/oop/popova_p_n/tack2/Player.java
@@ -0,0 +1,2 @@
+package ru.vsu.cs.oop.popova_p_n.tack2;public class Player {
+}
